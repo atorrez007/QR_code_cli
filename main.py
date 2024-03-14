f1cbfd308e2bd2a9849e53a2ebf649196fc4cd8f
“atorrez007” <xxtrm98@protonmail.com>
2024-03-14 12:07:37 -0700

    removed more functions and added randomint generator for multiple files

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import qrcode
+from random import randint
 
 
-# I need to find a way to continue looping main while maintaining state in the program until exit is called.
-
-# When the QR code is generated, the img is appended to the images list.
-images = []
-
-count = 0
-
-
-def add_count(count):
-    print(f"Previous count is {count}")
-
-    return count + 1
-
-
-# generate qr code and save in file
 def generate_qr(string):
     img = qrcode.make(string)
-    images.append(img)
-    img.save(f"user_qr_code_{count + 1}.png")
-    print("Your QR code has been generated!")
+    img.save(f"user_qr_code_{randint(1, 100)}.png")
 
 
 def main():
     user_string = input("Please enter a string to generate a QR code: ")
     generate_qr(user_string)
+    print("QR Code Generated!")
 
 
 if __name__ == "__main__":
