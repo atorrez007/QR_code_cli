5a9e2d014cf0401473047623ce4c526c699fb48a
“atorrez007” <xxtrm98@protonmail.com>
2024-03-14 09:11:11 -0700

    removed extra function. Maintaining count state for file differentiation is needed.

--- main.py
+++ main.py
@@ -2,43 +2,30 @@ import qrcode
 
 
 # I need to find a way to continue looping main while maintaining state in the program until exit is called.
-images = []
 
-# Code to keep png files in order
+# When the QR code is generated, the img is appended to the images list.
+images = []
 
 count = 0
 
 
 def add_count(count):
-    print(count)
-    return count
+    print(f"Previous count is {count}")
 
-
-def ask_for_input(user_input):
-    # Give user option to exit program
-    if user_input == "exit":
-        exit()
-
-    else:
-        user_input.lower()
-        generate_qr(user_input)
-        # add_count(count)
+    return count + 1
 
 
 # generate qr code and save in file
 def generate_qr(string):
     img = qrcode.make(string)
     images.append(img)
-    img.save(f"user_qr_code_{add_count(count)}.png")
+    img.save(f"user_qr_code_{count + 1}.png")
     print("Your QR code has been generated!")
 
 
 def main():
-    user_string = input(
-        "Please enter a string to generate a QR code or type exit to leave program: "
-    )
-    ask_for_input(user_string)
-    user_string.lower()
+    user_string = input("Please enter a string to generate a QR code: ")
+    generate_qr(user_string)
 
 
 if __name__ == "__main__":
