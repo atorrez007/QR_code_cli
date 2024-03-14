60e7369927ddab479753237616975cbf8d939f6e
“atorrez007” <xxtrm98@protonmail.com>
2024-03-14 12:28:54 -0700

    added customization on qrcode

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import qrcode
 from random import randint
 
 
-def generate_qr(string):
-    img = qrcode.make(string)
+def generate_qr(string, preference):
+    if preference.lower() == "hacker":
+        fill_choice = "green"
+        back_choice = "black"
+    else:
+        fill_choice = "black"
+        back_choice = "white"
+
+    # customization of qrcode class
+    qr = qrcode.QRCode(
+        version=12,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=6,
+        border=8,
+    )
+    qr.add_data(string)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=f"{fill_choice}", back_color=f"{back_choice}")
     img.save(f"user_qr_code_{randint(1, 100)}.png")
 
 
 def main():
     user_string = input("Please enter a string to generate a QR code: ")
-    generate_qr(user_string)
+    user_style = input("Are you a hacker or a civilian? ")
+    generate_qr(user_string, user_style)
     print("QR Code Generated!")
 
 
