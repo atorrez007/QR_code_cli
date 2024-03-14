14429cccd59728aef3b14f72b11aebe7a40b4e3d
“atorrez007” <xxtrm98@protonmail.com>
2024-03-14 13:00:07 -0700

    updated input prompts

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import qrcode
 from random import randint
 
 
-def generate_qr(string, preference):
-    if preference.lower() == "hacker":
+def generate_qr(user_string, preference):
+    if preference.lower().strip() == "h":
         fill_choice = "green"
         back_choice = "black"
     else:
@@ -17,15 +17,17 @@ def generate_qr(string, preference):
         box_size=6,
         border=8,
     )
-    qr.add_data(string)
+    qr.add_data(user_string)
     qr.make(fit=True)
     img = qr.make_image(fill_color=f"{fill_choice}", back_color=f"{back_choice}")
     img.save(f"user_qr_code_{randint(1, 100)}.png")
 
 
 def main():
-    user_string = input("Please enter a string to generate a QR code: ")
-    user_style = input("Are you a hacker or a civilian? ")
+    user_string = input("Please enter the data you want to encode: ")
+    user_style = input(
+        "Are you a hacker or a civilian? Type 'H' for Hacker and 'C' for Civilian: "
+    )
     generate_qr(user_string, user_style)
     print("QR Code Generated!")
 
@@ -34,7 +36,7 @@ if __name__ == "__main__":
     while True:
         main()
         new_command = input(
-            "Type Yes to create another QR Code or No to exit the program: "
+            "Do you want to generate a new QR Code? Type 'Y' for Yes and 'N' to Exit the Program: "
         )
-        if new_command.lower() != "yes":
+        if new_command.lower().strip() != "y":
             break
